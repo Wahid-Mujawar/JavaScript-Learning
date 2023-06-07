d5c1ea2b4a989865e518e29bf06805b4ce415c80
wmujawax <wahidx.mujawar@intel.com>
2023-06-07 16:15:19 +0530

    string-indexing trim toUppercase toLowercase slice

---
+++ f2-string-indexing.js
@@ -0,0 +1,10 @@
+//String indexing
+
+let firstName = "Javascript";
+
+//J a v a s c r i p t 
+//0 1 2 3 4 5 6 7 8 9
+
+console.log(firstName.length)   //length of string firstName
+
+console.log(firstName[firstName.length-2])  //last index : length - 1
---
+++ f3-trim-uppercase-lowercase-slice.js
@@ -0,0 +1,22 @@
+//trim
+//toUpperCase
+//toLowerCase
+//slice
+
+//trim
+let firstName = "JavaScript";
+console.log(firstName.length);  //length at initial
+
+firstName = firstName.trim(); //trims the spaces from string
+console.log(firstName); //print the trimmed string
+console.log(firstName.length); //length of new sting
+
+//toUpperCase
+console.log(firstName.toUpperCase());
+
+//toUpperCase
+console.log(firstName.toLowerCase());
+
+//slice
+firstName = firstName.slice(0,4);
+console.log(firstName);
