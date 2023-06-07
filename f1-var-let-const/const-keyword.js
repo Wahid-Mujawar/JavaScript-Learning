f8351bf26f119c4fe17c6f0936fc3c2316c4f5ec
wmujawax <wahidx.mujawar@intel.com>
2023-06-07 15:42:30 +0530

    const keyword

---
+++ f1-var-let-const/const-keyword.js
@@ -0,0 +1,7 @@
+//declare const as variable name
+//cannot change the value once declared variable
+
+const pi = 3.142;
+console.log(pi);
+// pi = 3.143        (invalid type error)
+// console.log(pi);
