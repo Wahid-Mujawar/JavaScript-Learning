a6a07ae5db1ac3af4233592fa08e16473170267b
wmujawax <wahidx.mujawar@intel.com>
2023-06-07 14:05:15 +0530

    var keyword

--- f1-var-let-const/var-keyword.js
+++ f1-var-let-const/var-keyword.js
@@ -5,11 +5,33 @@
 //We can change the information later
 
 
-//declare a variable
-var firstName = "Javascript";
-//use a variable
-console.log(firstName);
+var firstName = "Javascript"; //declare a variable
+console.log(firstName); //use a variable
 
 //change value
 firstName = "ES6:2015";
 console.log(firstName);
+
+
+//Rules for naming the variables
+
+//cannot start a var with number
+//Example
+//1firstName (invalid)
+//firstName1 (valid)
+
+//we can use $ or _ to start a variable (anyplace of the string)
+//Example
+//$firstName (valid)
+//first$Name (valid)
+
+//_firstName (valid)
+//first_Name (valid)
+
+//cannot uses spaces
+//first Name (invalid)
+
+//convention start with uppercase or lowercase
+//Example
+//firstName
+//firstname
