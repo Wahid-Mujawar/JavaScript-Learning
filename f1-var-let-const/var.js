9452a08f0b18686cdb742c719525db0f02bedaa2
wmujawax <wahidx.mujawar@intel.com>
2023-06-07 13:47:44 +0530

    var keyword

---
+++ f1-var-let-const/var.js
@@ -0,0 +1,15 @@
+//Intro to variables represented as var
+
+//var can store some information
+//The information can be used it later
+//We can change the information later
+
+
+//declare a variable
+var firstName = "Javascript";
+//use a variable
+console.log(firstName);
+
+//change value
+firstName = "ES6:2015";
+console.log(firstName);
